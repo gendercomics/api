3e89e05a4fdd0944332d856712f3b62e543811db
Michael Litschauer <michael.litschauer@ebcont.com>
2019-07-12 11:30:20 +0200

    now let's try deployments

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh root@gendercomics.net 'docker-compose up -f /root/gendercomics/docker-gendercomics/docker-compose.yml -d'
+ssh root@gendercomics.net 'docker pull gendercomics/api:latest && docker-compose up -f /root/gendercomics/docker-gendercomics/docker-compose.yml -d'
