72d4089f89c5ac011f597ed97951a130938be1ca
Michael Litschauer <michael.litschauer@ebcont.com>
2019-07-16 10:02:23 +0200

    API documentation update

--- src/main/java/net/gendercomics/api/controller/ComicController.java
+++ src/main/java/net/gendercomics/api/controller/ComicController.java
@@ -2,6 +2,8 @@ package net.gendercomics.api.controller;
 
 import java.util.List;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import net.gendercomics.api.data.service.ComicService;
 import net.gendercomics.api.model.Comic;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"comics"})
 @RestController
 @RequestMapping("/comics")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -17,6 +20,7 @@ public class ComicController {
 
     private final ComicService _comicService;
 
+    @ApiOperation("get all comics")
     @GetMapping
     public List<Comic> getAllComics() {
         return _comicService.findAll();
--- src/main/java/net/gendercomics/api/controller/CommonController.java
+++ src/main/java/net/gendercomics/api/controller/CommonController.java
@@ -1,19 +1,22 @@
 package net.gendercomics.api.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import net.gendercomics.api.model.ApiInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"common endpoints"})
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CommonController {
 
     private final ApiInfo _apiInfo;
 
-    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    @ApiOperation("retreive API information")
+    @GetMapping(path = "/info")
     public ApiInfo getInfo() {
         return _apiInfo;
     }
