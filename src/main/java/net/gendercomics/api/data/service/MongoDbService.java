0d4760db74d4b6e7a2be2144d7944dcc918e2156
Michael Litschauer <michael.litschauer@ebcont.com>
2020-02-17 16:33:59 +0100

    MongoDbService update

--- src/main/java/net/gendercomics/api/data/service/MongoDbService.java
+++ src/main/java/net/gendercomics/api/data/service/MongoDbService.java
@@ -2,11 +2,12 @@ package net.gendercomics.api.data.service;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
 import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +16,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class MongoDbService {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String _mongoURI;
+
     public MongoTemplate getMongoTemplate() {
-        MongoCredential credential = MongoCredential.createScramSha1Credential("gendercomics_api", "admin", new String("test123").toCharArray());
-        MongoClientOptions options = MongoClientOptions.builder().build();
-        MongoClient mongoClient = new MongoClient(new ServerAddress("localhost"), credential, options);
+        MongoClientURI mongoClientURI = new MongoClientURI(_mongoURI);
+        MongoClient mongoClient = new MongoClient(mongoClientURI);
 
         return new MongoTemplate(mongoClient, "gendercomics-dev");
     }
