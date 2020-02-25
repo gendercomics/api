4928fc21a8f1d5f6a4b78b0123148a585c01c9e5
Michael Litschauer <michael.litschauer@ebcont.com>
2020-02-25 13:21:53 +0100

    pseudonym for person

--- src/main/java/net/gendercomics/api/model/Person.java
+++ src/main/java/net/gendercomics/api/model/Person.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.util.StringUtils;
 
@@ -14,14 +15,28 @@ import org.springframework.util.StringUtils;
 public class Person implements Comparable<Person> {
 
     private String id;
-    @ApiModelProperty(value = "first name", required = true)
+
+    @ApiModelProperty(value = "first name")
     private String firstName;
-    @ApiModelProperty(value = "last name", required = true)
+
+    @ApiModelProperty(value = "last name")
     private String lastName;
-    @ApiModelProperty(value = "wikidata", required = true)
+
+    @ApiModelProperty(value = "wikidata")
     private String wikiData;
+
+    @ApiModelProperty(value = "pseudonym")
+    private String pseudonym;
+
     @ApiModelProperty(value = "metadata", required = true)
     private MetaData metaData;
+
+    private String getLastNameOrPseudonym() {
+        if (this.lastName != null) {
+            return this.lastName;
+        }
+        return this.pseudonym;
+    }
 
     @Override
     public String toString() {
@@ -30,7 +45,10 @@ public class Person implements Comparable<Person> {
 
     @Override
     public int compareTo(Person o) {
-        int last = this.lastName.compareToIgnoreCase(o.lastName);
+        int last = 0;
+        if (getLastNameOrPseudonym() != null) {
+            last = this.getLastNameOrPseudonym().compareToIgnoreCase(o.getLastNameOrPseudonym());
+        }
         return last == 0 ? this.firstName.compareToIgnoreCase(o.firstName) : last;
     }
 }
