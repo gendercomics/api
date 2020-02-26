8b7e69f1534b67e4e3fa41f1cf2ce39aae301cca
Michael Litschauer <michael.litschauer@ebcont.com>
2020-02-26 14:43:51 +0100

    cors annotation for CommonController

--- src/main/java/net/gendercomics/api/controller/CommonController.java
+++ src/main/java/net/gendercomics/api/controller/CommonController.java
@@ -11,11 +11,13 @@ import net.gendercomics.api.model.DataCount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = {"common endpoints"})
 @RestController
+@CrossOrigin
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CommonController {
 
