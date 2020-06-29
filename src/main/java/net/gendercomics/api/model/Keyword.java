07bd17f1383a84bbe1402667c937b97c36041479
Michael Litschauer <michael.litschauer@ebcont.com>
2020-06-29 21:10:24 +0200

    added unique index

--- src/main/java/net/gendercomics/api/model/Keyword.java
+++ src/main/java/net/gendercomics/api/model/Keyword.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Map;
@@ -22,6 +23,7 @@ public class Keyword {
     @ApiModelProperty(value = "keyword type (content)", required = true)
     private KeywordType type;
 
+    @Indexed(name = "keyword_value_index", unique = true)
     @ApiModelProperty(value = "list of keywords (one list entry per language)", required = true)
     private Map<Language, KeywordValue> values;
 
