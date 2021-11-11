a7dff13afe4a02ba1c4eea266555ccdbfe0ff216
michael litschauer <michael.litschauer@gmail.com>
2021-11-11 15:56:16 +0100

    empty SearchService class

---
+++ src/main/java/net/gendercomics/api/data/service/SearchService.java
@@ -0,0 +1,12 @@
+package net.gendercomics.api.data.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
+public class SearchService {
+}
