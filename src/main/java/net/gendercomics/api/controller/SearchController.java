0eb12c97db7f191f963092e0216dd46543652a70
michael litschauer <michael.litschauer@gmail.com>
2021-11-15 20:34:55 +0100

    elasticsearch dependencies and initial search controller

---
+++ src/main/java/net/gendercomics/api/controller/SearchController.java
@@ -0,0 +1,31 @@
+package net.gendercomics.api.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import net.gendercomics.api.data.service.SearchService;
+import net.gendercomics.api.model.Comic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Api(tags = {"search"})
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class SearchController {
+
+    private final SearchService _searchService;
+
+    @ApiOperation("insert a comic")
+    @PostMapping(path = "/search/comics/title")
+    public List<Comic> findComicsByTitle(@ApiParam(required = true) @RequestBody String searchTerm) {
+        return _searchService.search(searchTerm);
+    }
+
+}
