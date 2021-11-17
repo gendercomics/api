aa0c606b68c5949b81f40e5df24bf095751a90b8
michael litschauer <michael.litschauer@gmail.com>
2021-11-17 20:47:59 +0100

    just better readability

--- src/main/java/net/gendercomics/api/model/MetaData.java
+++ src/main/java/net/gendercomics/api/model/MetaData.java
@@ -2,6 +2,7 @@ package net.gendercomics.api.model;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -14,12 +15,16 @@ public class MetaData {
 
     @ApiModelProperty(value = "Date on which the entry was created", required = true)
     private Date createdOn;
+
     @ApiModelProperty(value = "User who created the entry", required = true)
     private String createdBy;
+
     @ApiModelProperty(value = "Date on which the entry was modified")
     private Date changedOn;
+
     @ApiModelProperty(value = "User who modified the entry")
     private String changedBy;
+
     @ApiModelProperty(value = "Status of entry: DRAFT|REVIEW|FINAL, default value = DRAFT")
     private Status status = Status.DRAFT;
 }
