db776146cb7e32316a554fd169dbd71cc24fa186
michael litschauer <michael.litschauer@gmail.com>
2021-11-30 15:03:37 +0100

    changed formatting

--- src/main/java/net/gendercomics/api/data/service/FileService.java
+++ src/main/java/net/gendercomics/api/data/service/FileService.java
@@ -4,6 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
     void save(String comicId, MultipartFile file);
-
     void delete(String comicId, String fileName);
 }
