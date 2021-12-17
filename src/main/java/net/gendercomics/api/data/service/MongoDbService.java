760c334d099a554c6c8fb27b3ba097582f5ae128
michael litschauer <michael.litschauer@gmail.com>
2021-12-17 08:50:01 +0100

    class not really needed, leave it here for documentation purposes

--- src/main/java/net/gendercomics/api/data/service/MongoDbService.java
+++ src/main/java/net/gendercomics/api/data/service/MongoDbService.java
@@ -2,6 +2,7 @@ package net.gendercomics.api.data.service;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,10 @@ public class MongoDbService {
     public MongoTemplate getMongoTemplate() {
         MongoClient mongoClient = MongoClients.create(_mongoURI);
         return new MongoTemplate(mongoClient, "gendercomics-dev");
+    }
+
+    public MongoDatabase getDb(String dbName) {
+        MongoClient mongoClient = MongoClients.create(_mongoURI);
+        return mongoClient.getDatabase(dbName);
     }
 }
