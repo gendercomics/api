ca5c736c3f1515d48bd9701f2b2820c75d2d7f3d
michael litschauer <michael.litschauer@gmail.com>
2022-03-17 11:39:03 +0100

    relations: wip

--- src/test/java/net/gendercomics/api/integrationtest/MigrationServiceIntegrationTest.java
+++ src/test/java/net/gendercomics/api/integrationtest/MigrationServiceIntegrationTest.java
@@ -1,21 +1,11 @@
 package net.gendercomics.api.integrationtest;
 
-import net.gendercomics.api.data.service.impl.ComicServiceImpl;
 import net.gendercomics.api.data.service.MigrationService;
 import net.gendercomics.api.data.service.TextService;
-import net.gendercomics.api.model.Comic;
-import net.gendercomics.api.model.MigrationResult;
-import net.gendercomics.api.model.Text;
+import net.gendercomics.api.data.service.impl.ComicServiceImpl;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
-import java.util.ArrayList;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MigrationServiceIntegrationTest extends AbstractIntegrationTest {
 
@@ -30,28 +20,6 @@ public class MigrationServiceIntegrationTest extends AbstractIntegrationTest {
 
     @Autowired
     private MongoTemplate _mongo;
-
-    @Test
-    public void whenComicCommentToRelation_ThenOk() {
-        Text text = new Text();
-        text.setValue("hello gendercomics");
-        text = _textService.save(text, "integrationtest");
-
-        Comic comic = new Comic();
-        comic.setComments(new ArrayList<>());
-        comic.getComments().add(text);
-        comic = _comicService.save(comic, "integrationtest");
-
-        MigrationResult result = _migrationService.comicCommentToRelation();
-        assertEquals("OK", result.getStatus());
-        assertNotNull(result.getSource());
-        assertEquals(1, result.getSource().size());
-        assertNotNull(result.getResult());
-        assertEquals(1, result.getResult().size());
-
-        Comic resultComic = _comicService.getComic(comic.getId());
-       // assertThat(resultComic.getTextCommentsComic().get(0).getValue()).isEqualTo("hello gendercomics");
-    }
 
     @AfterEach
     private void cleanup() {
