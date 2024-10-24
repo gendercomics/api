8417a4f9b97d15986585db63da58b5746f866229
michael litschauer <michael.litschauer@gmail.com>
2024-10-24 21:31:41 +0200

    toplevel keywords fetched (criteria=no outgoing relations, status=FINAL)

--- src/main/java/net/gendercomics/api/data/service/impl/KeywordServiceImpl.java
+++ src/main/java/net/gendercomics/api/data/service/impl/KeywordServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
